e94e7721e3b488e35fae39c23dd8b479da1dda49
Kristofer Peterson <svenski123@users.noreply.github.com>
2025-03-11 00:03:32 +0000

    Clean up

--- hw/hw12/lvl0/src/Lvl0.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
-
-contract Lvl0 {
-    fallback(bytes calldata) external payable returns (bytes memory) {
-	return abi.encode("uint8", 42);
-    }
-}
